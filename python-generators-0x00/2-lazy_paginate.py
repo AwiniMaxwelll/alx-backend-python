c53f30403dc4b898dead5955bc48f63e0ebb43b0
MAXWELL AWINI <awinimaxwell428@gmail.com>
2025-09-13 23:48:39 +0000

    Simulte fetching paginated data from the users database using a generator to lazily load each page

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,25 @@
+from seed import create_db
+def paginate_users(page_size, offset):
+    """
+Simulate fetching paginated data from the users database using a generator to lazily load each page
+
+Implement a generator function lazypaginate(pagesize) that implements the paginate_users(page_size, offset) that will only fetch the next page when needed at an offset of 0.
+"""
+    connection = create_db().connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+
+
+
+def lazy_paginate(page_size):
+    offset = 0
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
