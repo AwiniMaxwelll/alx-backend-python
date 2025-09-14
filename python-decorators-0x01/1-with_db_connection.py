342235f69a69e5242df71445461f398a772727d0
MAXWELL AWINI <awinimaxwell428@gmail.com>
2025-09-14 22:21:18 +0000

    a decorator that automatically handles opening and closing database connections

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,28 @@
+import functools
+import sqlite3
+
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        connection =sqlite3.connect("users.db")
+        try:
+            result = func(connection, *args, **kwargs)
+            connection.commit()
+            return result
+        except Exception as e:
+            connection.rollback()
+            raise e
+        finally:
+            connection.close()
+    return wrapper
+
+@with_db_connection
+def get_user_by_id(conn, user_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+    return cursor.fetchone()
+
+# Fetch user by ID with automatic connection handling
+user = get_user_by_id(user_id=1)
+print(user)
